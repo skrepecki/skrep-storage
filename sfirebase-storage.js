583d428bd148242df5716bbda4b980438439a40d
skrepecki <75933673+skrepecki@users.noreply.github.com>
2022-11-27 02:10:45 +0100

    Update sfirebase-storage.js

--- sfirebase-storage.js
+++ sfirebase-storage.js
@@ -5,5 +5,11 @@ const streamHosting = [
     ],[
         'Невзоров, Гудков, Навальный',
         'https://firebasestorage.googleapis.com/v0/b/skrepecki.appspot.com/o/0.mp4?alt=media&token=9af25139-d825-49a8-9103-b3a8a9ba1e68'
+    ],[
+        'Навальный и фбк выступили против Украины в поддержку русских оккупантов!',
+        'https://firebasestorage.googleapis.com/v0/b/skrep2-storage.appspot.com/o/1.mp4?alt=media&token=52a12219-8aa3-4b03-9bf1-333c04e0b136'
+    ],[
+        'Невзоров, Гудков, Навальный',
+        'https://firebasestorage.googleapis.com/v0/b/skrepecki.appspot.com/o/0.mp4?alt=media&token=9af25139-d825-49a8-9103-b3a8a9ba1e68'
     ]
 ]
