d02d525704bcb57b1906c01b1cc472ecc2c7c65d
СЕМЕН СКРЕПЕЦКИЙ КАРТИНЫ <75933673+skrepecki@users.noreply.github.com>
2022-12-25 21:19:40 +0100

    Update sfirebase-storage.js

--- sfirebase-storage.js
+++ sfirebase-storage.js
@@ -2,16 +2,12 @@ const streamHosting = [
     [
         'Пионтковский',
         'https://skrepecki.github.io/skrepecki/public/wordpress/skrep-img/video-piontkovski.mp4'
-    ],
-    [
+    ],[
         'Пионтковский',
         'https://drive.google.com/file/d/1gyGq08vTVsutH0I_K9FmZGa8sRpeKAaB/view'
     ],[
         'Невзоров, Гудков, Навальный',
         'https://firebasestorage.googleapis.com/v0/b/skrepecki.appspot.com/o/0.mp4?alt=media&token=9af25139-d825-49a8-9103-b3a8a9ba1e68'
-    ],[
-        'Навальный и фбк выступили против Украины в поддержку русских оккупантов!',
-        'https://firebasestorage.googleapis.com/v0/b/skrep2-storage.appspot.com/o/1.mp4?alt=media&token=52a12219-8aa3-4b03-9bf1-333c04e0b136'
     ],[
         'Невзоров, Гудков, Навальный',
         'https://firebasestorage.googleapis.com/v0/b/skrepecki.appspot.com/o/0.mp4?alt=media&token=9af25139-d825-49a8-9103-b3a8a9ba1e68'
