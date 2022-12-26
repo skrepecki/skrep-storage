ac94ce678479f86abc7440eded7e4dc6c2e12f16
СЕМЕН СКРЕПЕЦКИЙ КАРТИНЫ <75933673+skrepecki@users.noreply.github.com>
2022-12-26 19:07:39 +0100

    Update sfirebase-storage.js

--- sfirebase-storage.js
+++ sfirebase-storage.js
@@ -4,12 +4,12 @@ const streamHosting = [
         'https://skrepecki.github.io/skrepecki/public/wordpress/skrep-img/video-piontkovski.mp4'
     ],[
         'Пионтковский',
-        'https://drive.google.com/file/d/1gyGq08vTVsutH0I_K9FmZGa8sRpeKAaB/view'
+        'https://skrepecki.github.io/skrepecki/public/wordpress/skrep-img/video-piontkovski.mp4'
     ],[
-        'Невзоров, Гудков, Навальный',
-        'https://firebasestorage.googleapis.com/v0/b/skrepecki.appspot.com/o/0.mp4?alt=media&token=9af25139-d825-49a8-9103-b3a8a9ba1e68'
+        'Пионтковский',
+        'https://skrepecki.github.io/skrepecki/public/wordpress/skrep-img/video-piontkovski.mp4'
     ],[
-        'Невзоров, Гудков, Навальный',
-        'https://firebasestorage.googleapis.com/v0/b/skrepecki.appspot.com/o/0.mp4?alt=media&token=9af25139-d825-49a8-9103-b3a8a9ba1e68'
+        'Пионтковский',
+        'https://skrepecki.github.io/skrepecki/public/wordpress/skrep-img/video-piontkovski.mp4'
     ]
 ]
